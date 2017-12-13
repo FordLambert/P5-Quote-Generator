db6c0cfdfb01f0c89499f70422616f5c87744551
FordLambert <ford.lambert@protonmail.ch>
2017-12-13 11:13:57 +0100

    Remove step 1, rename folders
    Step 1 had been removed, it was no longer usefull to had it here,
    folder's name had been changed: it is now more obvious which one do
    what.

--- 1 - first step/js/quote.js
+++
@@ -1,5 +0,0 @@
-var Quote = function(beginning, middle, end){
-	this.beginning = beginning;
-	this.middle = middle;
-	this.end = end;
-};
--- 1 - first step/js/store.js
+++
@@ -1,5 +0,0 @@
-var QuoteFragmentsStore = function(beginnings, middles, ends){
-    this.beginnings = beginnings;
-    this.middles = middles;
-    this.ends = ends;
-};
